1acc90d76c9403df2f81702e6b91458414d0fed7
Jacky Nguyen <nktpro@gmail.com>
2021-11-15 23:13:35 -0500

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,15 @@
 {
   description = "Misc Nix packages";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    flakeUtils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]
+  outputs = { self, nixpkgs, flakeUtils }:
+    flakeUtils.lib.eachSystem [ "aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux" ]
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
