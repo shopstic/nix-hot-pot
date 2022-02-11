72715caf2caf4ab2c4bc54d2e3a127eef6e3db7f
Jacky Nguyen <nktpro@gmail.com>
2022-02-10 23:15:09 -0500

    Fix rev

--- pkgs/regclient.nix
+++ pkgs/regclient.nix
@@ -8,7 +8,7 @@ pkgs.callPackage "${pkgs.path}/pkgs/development/tools/regclient" {
     src = pkgs.fetchFromGitHub {
       owner = "regclient";
       repo = "regclient";
-      rev = version;
+      rev = "v${version}";
       sha256 = "sha256-3nYVhKHNz0V0j6JlZ5Dm5TFWA2kmUhshNVUym/QWSyM=";
     };
     vendorSha256 = "sha256-rj4sQ8Ci2KMayJNXn+KVihOiZehk233l48Ps0yjOOE4=";
