e0eb34a9afb0a2ae44eb25f2f28712010ea20cc3
Jacky Nguyen <nktpro@gmail.com>
2022-05-01 23:24:26 -0400

    Fix regclient vcstag

--- pkgs/regclient.nix
+++ pkgs/regclient.nix
@@ -3,15 +3,21 @@ let
   version = "0.4.2";
 in
 pkgs.callPackage "${pkgs.path}/pkgs/development/tools/regclient" {
-  buildGoModule = args: pkgs.buildGoModule (args // {
+  buildGoModule = args: pkgs.buildGoModule (args // rec {
     inherit version;
+    tag = "v${version}";
     src = pkgs.fetchFromGitHub {
       owner = "regclient";
       repo = "regclient";
-      rev = "v${version}";
+      rev = tag;
       sha256 = "sha256-lx2IQ3NpFuVr4Vb7vFcp/QVZBlLzi4VXFE7Sw3LKIXE=";
     };
     vendorSha256 = "sha256-9DcmUYwG+CoT8hmoI7FHl7td1bBukpN/FJT3iiCAkHU=";
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.VCSTag=${tag}"
+    ];
     checkPhase = "";
   });
 }
