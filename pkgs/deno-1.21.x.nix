2bee410a07310852ca65714c0da3169f6dce4355
Jacky Nguyen <nktpro@gmail.com>
2022-05-11 20:55:56 -0400

    Update deno to 1.21.2

--- pkgs/deno-1.21.x.nix
+++ pkgs/deno-1.21.x.nix
@@ -1,23 +1,23 @@
 { autoPatchelfHook, fetchzip, stdenv }:
 (import ./deno.nix rec {
   inherit autoPatchelfHook fetchzip stdenv;
-  version = "1.21.1";
+  version = "1.21.2";
   downloadMap = {
     x86_64-linux = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/deno-x86_64-unknown-linux-gnu.zip";
-      hash = "sha256-FxuRAHRp6TcaWedPAPAbcc0+ooH79yR3rmLriEWZZm8=";
+      hash = "sha256-nCZ9bm4YJXJdzsOZjy4LU30kGbC7poh/pR1fLREIXM0=";
     };
     x86_64-darwin = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/deno-x86_64-apple-darwin.zip";
-      hash = "sha256-WTHCLbrYVtrKHu8upUHCj9GrE6M3Y67M9T7wPA5pBGI=";
+      hash = "sha256-BKs/v+R4IRn48Q5oAdRNb1ih3MnZK98EonwzAQd6yaQ=";
     };
     aarch64-darwin = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/deno-aarch64-apple-darwin.zip";
-      hash = "sha256-HzKchkW7M5ErjdohR3+9wXms7CA2hWstuHtjjwPaAsw=";
+      hash = "sha256-bxAUdTL1oRNVSVN71dNOgdHexhIeAWcS7y0cAKdUxd0=";
     };
     aarch64-linux = {
       url = "https://github.com/LukeChannings/deno-arm64/releases/download/v${version}/deno-linux-arm64.zip";
-      hash = "sha256-prqdzTXrbzhHP/wUvIsiTwG1vzXULevTcUJcZZD31lA=";
+      hash = "sha256-r6Zsq6AYxly520dLmu4Zw7BrK9AU9cJCzCFlezDaxDc=";
     };
   };
   priority = 10;
