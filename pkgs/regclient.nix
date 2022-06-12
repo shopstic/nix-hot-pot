ef092c1fcca468f2933ad9d29a5a12f0ce4d4a36
Jacky Nguyen <nktpro@gmail.com>
2022-06-12 03:14:58 -0400

    Fix regclient

--- pkgs/regclient.nix
+++ pkgs/regclient.nix
@@ -1,23 +1,42 @@
-{ pkgs }:
-let
+{ stdenv, lib, buildGoModule, fetchFromGitHub }:
+let bins = [ "regbot" "regctl" "regsync" ]; in
+buildGoModule rec {
+  pname = "regclient";
   version = "0.4.2";
-in
-pkgs.callPackage "${pkgs.path}/pkgs/development/tools/regclient" {
-  buildGoModule = args: pkgs.buildGoModule (args // rec {
-    inherit version;
-    tag = "v${version}";
-    src = pkgs.fetchFromGitHub {
-      owner = "regclient";
-      repo = "regclient";
-      rev = tag;
-      sha256 = "sha256-lx2IQ3NpFuVr4Vb7vFcp/QVZBlLzi4VXFE7Sw3LKIXE=";
-    };
-    vendorSha256 = "sha256-9DcmUYwG+CoT8hmoI7FHl7td1bBukpN/FJT3iiCAkHU=";
-    ldflags = [
-      "-s"
-      "-w"
-      "-X main.VCSTag=${tag}"
-    ];
-    checkPhase = "";
-  });
+  tag = "v${version}";
+
+  src = fetchFromGitHub {
+    owner = "regclient";
+    repo = "regclient";
+    rev = tag;
+    sha256 = "sha256-lx2IQ3NpFuVr4Vb7vFcp/QVZBlLzi4VXFE7Sw3LKIXE=";
+  };
+  vendorSha256 = "sha256-7kDl7gyKFM2gErDhOBCo6T4etwsAJnRpVtaJgv6BefM=";
+
+  outputs = [ "out" ] ++ bins;
+
+  ldflags = [
+    "-s"
+    "-w"
+    "-X main.VCSTag=${tag}"
+  ];
+  checkPhase = "";
+
+  postInstall =
+    lib.concatStringsSep "\n" (
+      map
+        (bin: ''
+          mkdir -p ''$${bin}/bin &&
+          mv $out/bin/${bin} ''$${bin}/bin/ &&
+          ln -s ''$${bin}/bin/${bin} $out/bin/
+        '')
+        bins
+    );
+
+  meta = with lib; {
+    description = "Docker and OCI Registry Client in Go and tooling using those libraries";
+    homepage = "https://github.com/regclient/regclient";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ];
+  };
 }
