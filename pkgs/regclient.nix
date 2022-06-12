bebab2e70001bb850c7b4701f6e6a8aeee48d71a
Jacky Nguyen <nktpro@gmail.com>
2022-06-12 03:12:18 -0400

    Revert back regclient to 0.4.2 since 0.4.3 has a config loading regression

--- pkgs/regclient.nix
+++ pkgs/regclient.nix
@@ -1,42 +1,23 @@
-{ stdenv, lib, buildGoModule, fetchFromGitHub }:
-let bins = [ "regbot" "regctl" "regsync" ]; in
-buildGoModule rec {
-  pname = "regclient";
-  version = "0.4.3";
-  tag = "v${version}";
-
-  src = fetchFromGitHub {
-    owner = "regclient";
-    repo = "regclient";
-    rev = tag;
-    sha256 = "sha256-CB/a66xNVzmPCoh0yLnOAB/htdp8QkE3MnwH7G2jqSU=";
-  };
-  vendorSha256 = "sha256-QTeVgvjRw2wBd8QGoVpRVTTh+Wwu2NEZYR1Z9R52/p0=";
-
-  outputs = [ "out" ] ++ bins;
-
-  ldflags = [
-    "-s"
-    "-w"
-    "-X main.VCSTag=${tag}"
-  ];
-  checkPhase = "";
-
-  postInstall =
-    lib.concatStringsSep "\n" (
-      map
-        (bin: ''
-          mkdir -p ''$${bin}/bin &&
-          mv $out/bin/${bin} ''$${bin}/bin/ &&
-          ln -s ''$${bin}/bin/${bin} $out/bin/
-        '')
-        bins
-    );
-
-  meta = with lib; {
-    description = "Docker and OCI Registry Client in Go and tooling using those libraries";
-    homepage = "https://github.com/regclient/regclient";
-    license = licenses.asl20;
-    maintainers = with maintainers; [ ];
-  };
+{ pkgs }:
+let
+  version = "0.4.2";
+in
+pkgs.callPackage "${pkgs.path}/pkgs/development/tools/regclient" {
+  buildGoModule = args: pkgs.buildGoModule (args // rec {
+    inherit version;
+    tag = "v${version}";
+    src = pkgs.fetchFromGitHub {
+      owner = "regclient";
+      repo = "regclient";
+      rev = tag;
+      sha256 = "sha256-lx2IQ3NpFuVr4Vb7vFcp/QVZBlLzi4VXFE7Sw3LKIXE=";
+    };
+    vendorSha256 = "sha256-9DcmUYwG+CoT8hmoI7FHl7td1bBukpN/FJT3iiCAkHU=";
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.VCSTag=${tag}"
+    ];
+    checkPhase = "";
+  });
 }
