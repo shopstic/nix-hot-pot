cd7d284802dfed6c72a6ea6ee49c64057d2fe17b
Jacky Nguyen <nktpro@gmail.com>
2022-08-11 01:21:05 -0400

    Simplify actions-runner-dind-nix

--- images/actions-runner-dind-nix/default.nix
+++ images/actions-runner-dind-nix/default.nix
@@ -21,19 +21,9 @@ dockerTools.buildLayeredImage
   name = "${baseName}-nix";
   fromImage = baseImage;
   tag = "${arcVersion}-${nix.version}";
-  contents = [
-    (
-      writeTextDir "etc/nix/nix.conf" ''
-        sandbox = false
-        substituters = https://cache.nixos.org?priority=40 https://nix-community.cachix.org?priority=50 https://nix.shopstic.com?priority=60
-        trusted-public-keys = nix-cache:jxOpK2dQOv/7JIb5/30+W4oidtUgmFMXLc/3mC09mKM= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
-        experimental-features = nix-command flakes
-      ''
-    )
-  ];
+  contents = [ nix ];
   config = {
     Env = [
-      "PATH=${lib.makeBinPath [ nix ]}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/runner/.local/bin"
       "GIT_SSL_CAINFO=/etc/ssl/certs/ca-certificates.crt"
       "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt"
     ];
