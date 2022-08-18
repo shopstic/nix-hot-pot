1dc8c1d8fe227fdd26c27ffcfdc083c25626b8e3
Jacky Nguyen <nktpro@gmail.com>
2022-08-18 08:23:12 -0400

    Update actions-runner-dind-nix image

--- images/actions-runner-dind-nix/default.nix
+++ images/actions-runner-dind-nix/default.nix
@@ -7,12 +7,12 @@
 }:
 let
   baseName = "actions-runner-dind";
-  arcVersion = "2.294.0";
+  arcVersion = "2.295.0";
   baseImage = dockerTools.pullImage {
     imageName = "docker.io/summerwind/actions-runner-dind";
-    imageDigest = "sha256:38295a73790ca6ec5c466afaf2781f9f6ba2a358ba8e80f5334ea2b4aebebc4e";
-    sha256 = "sha256-vWTum/cGMXrec55/bM4xVCxnrCa1/OrmasoPVsGGSdQ=";
-    finalImageTag = "v${arcVersion}-ubuntu-20.04-fc55477";
+    imageDigest = "sha256:1c4c7b8e125d51bcf6f6c5777eb515a76b12201aa36a2fe98543f71a8702913b";
+    sha256 = "sha256-0dkBF9HBaottGeURDvFA2LcK7pz2wtoUoqen5t4QBiA=";
+    finalImageTag = "v${arcVersion}-ubuntu-20.04-3724b46";
     finalImageName = baseName;
   };
   pathEnv = "${lib.makeBinPath [ nix ]}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/runner/.local/bin";
