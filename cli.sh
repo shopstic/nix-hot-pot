8c42721dbd496b4929690378aeb9af85828c14c8
Jacky Nguyen <nktpro@gmail.com>
2022-08-20 07:42:28 -0400

    Update CI jobs

--- cli.sh
+++ cli.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-build_push_images() {
+get_current_arch() {
+  local IMAGE_ARCH
+  IMAGE_ARCH=$(uname -m) || exit $?
+  if [[ "${IMAGE_ARCH}" == "arm64" ]]; then
+  IMAGE_ARCH="aarch64"
+  fi
+  echo "${IMAGE_ARCH}"
+}
+
+build_push_all_single_arch_images() {
+  local ARCH=${1:?"Arch is required (amd64 | arm64"}
   readarray -t IMAGES < <(find ./images -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 
   parallel -j2 --tagstring "[{}]" --line-buffer --retries=2 \
-    "$0" build_push_multi_arch {} ::: "${IMAGES[@]}"
+    "$0" build_push_single_arch {} ::: "${IMAGES[@]}" "${ARCH}"
 }
 
-build_push_multi_arch() {
-  local IMAGE=${1:?"Image name is required"}
-  local IMAGE_TAG
-  IMAGE_TAG=$(nix eval --raw ".#packages.x86_64-linux.image-${IMAGE}.imageTag") || exit $?
+push_all_manifests() {
+  readarray -t IMAGES < <(find ./images -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 
   parallel -j2 --tagstring "[{}]" --line-buffer --retries=2 \
-    "$0" build_push_single_arch "${IMAGE}" {} "${IMAGE_TAG}" ::: \
-    amd64 arm64
-
-  "$0" push_manifest "${IMAGE}" "${IMAGE_TAG}"
+    "$0" push_manifest {} ::: "${IMAGES[@]}"
 }
 
 build_push_single_arch() {
@@ -25,7 +30,13 @@ build_push_single_arch() {
 
   local IMAGE=${1:?"Image name is required"}
   local ARCH=${2:?"Arch is required (amd64 | arm64"}
-  local IMAGE_TAG=${3:?"Image tag is required"}
+  
+  local IMAGE_ARCH
+  IMAGE_ARCH=$("$0" get_current_arch) || exit $?
+
+  local IMAGE_TAG
+  IMAGE_TAG=$(nix eval --raw ".#packages.${IMAGE_ARCH}-linux.image-${IMAGE}.imageTag") || exit $?
+
   local TEMP_DIR
 
   TEMP_DIR=$(mktemp -d)
@@ -50,7 +61,12 @@ build_push_single_arch() {
 push_manifest() {
   local IMAGE_REPOSITORY=${IMAGE_REPOSITORY:?"IMAGE_REPOSITORY env var is required"}
   local IMAGE=${1:?"Image name is required"}
-  local IMAGE_TAG=${2:?"Image tag is required"}
+  local IMAGE_ARCH
+  local IMAGE_TAG
+
+  IMAGE_ARCH=$("$0" get_current_arch) || exit $?
+  IMAGE_TAG=$(nix eval --raw ".#packages.${IMAGE_ARCH}-linux.image-${IMAGE}.imageTag") || exit $?
+
   local TARGET="${IMAGE_REPOSITORY}/${IMAGE}:${IMAGE_TAG}"
   
   >&2 echo "Writing manifest for ${TARGET}"
