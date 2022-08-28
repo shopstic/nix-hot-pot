eca6c7127bd06f42fcf2422175cf5848a5567776
Jacky Nguyen <nktpro@gmail.com>
2022-08-28 13:13:48 -0400

    Update image-netcat with bash

--- images/netcat/default.nix
+++ images/netcat/default.nix
@@ -1,14 +1,18 @@
 { nix2container
 , netcat
 , dumb-init
+, bash
 }:
 nix2container.buildImage
 {
   name = "netcat";
   tag = netcat.version;
-  copyToRoot = [ netcat ];
+  copyToRoot = [ dumb-init netcat bash ];
   config = {
-    Entrypoint = [ "${dumb-init}/bin/dumb-init" "--" "${netcat}/bin/nc" ];
+    env = [
+      "PATH=/bin"
+    ];
+    entrypoint = [ "dumb-init" "--" "nc" ];
   };
 }
 
