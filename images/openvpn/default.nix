7f3befdcda0fbf376de75ed0d0c03a3ad1d74e9a
Jacky Nguyen <nktpro@gmail.com>
2022-09-01 11:47:09 -0400

    Add more tools to openvpn image

--- images/openvpn/default.nix
+++ images/openvpn/default.nix
@@ -5,12 +5,27 @@
 , openvpn
 , netcat
 , iptables
+, sysctl
+, iproute2
+, gnugrep
+, dig
 }:
 nix2container.buildImage
 {
   name = openvpn.name;
   tag = openvpn.version;
-  copyToRoot = [ dumb-init bash coreutils openvpn netcat iptables ];
+  copyToRoot = [
+    dumb-init
+    bash
+    coreutils
+    openvpn
+    netcat
+    iptables
+    sysctl
+    iproute2
+    gnugrep
+    dig
+  ];
   config = {
     env = [
       "PATH=/bin"
