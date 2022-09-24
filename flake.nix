c5dd4aa93843f619ae48278f22d894030e66b4a8
Jacky Nguyen <nktpro@gmail.com>
2022-09-24 13:27:55 -0400

    Remove obsolete deno versions

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     fdbPkg.url = "github:shopstic/nix-fdb/21ac866940d56f3016c0978f6ddc61f45d0088d6";
     flakeUtils.url = "github:numtide/flake-utils";
     npmlock2nixPkg = {
-      url = "github:nix-community/npmlock2nix/master";
+      url = "github:nix-community/npmlock2nix/5c4f247688fc91d665df65f71c81e0726621aaa8";
       flake = false;
     };
     nix2containerPkg = {
@@ -37,14 +37,6 @@
           nix2containerUtil = nix2containerPkgs.nix2containerUtil;
           fdb = fdbPkg.packages.${system}.fdb_7;
           fdbLib = fdb.lib;
-          deno_1_13_x = pkgs.callPackage ./pkgs/deno-1.13.x.nix { };
-          deno_1_16_x = pkgs.callPackage ./pkgs/deno-1.16.x.nix { };
-          deno_1_17_x = pkgs.callPackage ./pkgs/deno-1.17.x.nix { };
-          deno_1_18_x = pkgs.callPackage ./pkgs/deno-1.18.x.nix { };
-          deno_1_19_x = pkgs.callPackage ./pkgs/deno-1.19.x.nix { };
-          deno_1_20_x = pkgs.callPackage ./pkgs/deno-1.20.x.nix { };
-          deno_1_21_x = pkgs.callPackage ./pkgs/deno-1.21.x.nix { };
-          deno_1_22_x = pkgs.callPackage ./pkgs/deno-1.22.x.nix { };
           deno_1_23_x = pkgs.callPackage ./pkgs/deno-1.23.x.nix { };
           deno_1_24_x = pkgs.callPackage ./pkgs/deno-1.24.x.nix { };
           deno_1_25_x = pkgs.callPackage ./pkgs/deno-1.25.x.nix { };
@@ -133,7 +125,7 @@
             in
             {
               inherit
-                deno deno_1_13_x deno_1_16_x deno_1_17_x deno_1_18_x deno_1_19_x deno_1_20_x deno_1_21_x deno_1_22_x deno_1_23_x deno_1_24_x deno_1_25_x
+                deno deno_1_23_x deno_1_24_x deno_1_25_x
                 intellij-helper manifest-tool jdk17 jre17
                 skopeo-nix2container nix2containerUtil
                 karpenter oras;
