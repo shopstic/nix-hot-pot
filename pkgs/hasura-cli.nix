f3a8c5a2644a93a558cf8ea08223ccabfd533ae4
Jacky Nguyen <nktpro@gmail.com>
2022-10-07 22:48:54 -0400

    [hasura-cli] Update to 2.12.0

--- pkgs/hasura-cli.nix
+++ pkgs/hasura-cli.nix
@@ -1,22 +1,18 @@
 { fetchurl, stdenv }:
 let
-  version = "2.2.0";
+  version = "2.12.0";
   downloadMap = {
     x86_64-linux = {
       url = "https://github.com/hasura/graphql-engine/releases/download/v${version}/cli-hasura-linux-amd64";
-      hash = "sha256-Xzzb0AJXJB9gYgaqET4UINI7uQ0wSnD6oe1nGZEhSKU=";
-    };
-    x86_64-darwin = {
-      url = "https://github.com/hasura/graphql-engine/releases/download/v${version}/cli-hasura-darwin-amd64";
-      hash = "sha256-LO6yvE9fbsG7nBgxJ3Td+La38bTKHVQAPjPHXhp2mqg=";
+      hash = "sha256-D61PQ1oNkm6EEuZ4mzMlCCMRYk8hIhMKolZO7Ty+OI0=";
     };
     aarch64-darwin = {
       url = "https://github.com/hasura/graphql-engine/releases/download/v${version}/cli-hasura-darwin-arm64";
-      hash = "sha256-s2PL+d7/jf+sT7iTh+7WRJ/0/7RZfjl6TX6CN5lTMH8=";
+      hash = "sha256-hUd42pWwcqFqXKnokWyvL6K7EYtArVjhEYEWAATzMnM=";
     };
     aarch64-linux = {
       url = "https://github.com/hasura/graphql-engine/releases/download/v${version}/cli-hasura-linux-arm64";
-      hash = "sha256-3zr7Q7WPF9fyojfbjZ2P1EM/TmbqFhogfoZ7QU7tNok=";
+      hash = "sha256-vEvbTpnlIIuYRikPLht+vwm1kZHlpjMMzwjm2fhBuXE=";
     };
   };
 in
