b9618216780e7915a4559ae5cee2cc9337dd79c5
Jacky Nguyen <nktpro@gmail.com>
2023-03-29 18:21:02 -0400

    [lib] Expose denoAppCompile

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,7 @@
 {
   buildahBuild = import ./buildah-build.nix;
   denoAppBuild = import ./deno-app-build.nix;
+  denoAppCompile = import ./deno-app-compile.nix;
   wrapJdk = import ./wrap-jdk.nix;
   writeTextFiles = import ./write-text-files.nix;
   nonRootShadowSetup = import ./non-root-shadow-setup.nix;
