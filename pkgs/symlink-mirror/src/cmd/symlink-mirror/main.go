17ec6faac330c631308687b35262e6174865b149
Jacky Nguyen <nktpro@gmail.com>
2023-05-06 17:17:30 -0400

    In symlink-mirror, add support for an absolute flag

--- pkgs/symlink-mirror/src/cmd/symlink-mirror/main.go
+++ pkgs/symlink-mirror/src/cmd/symlink-mirror/main.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 	"sync"
 )
 
-func createSymlink(src, dst string) error {
-	relPath, err := filepath.Rel(filepath.Dir(dst), filepath.Dir(src))
-	if err != nil {
-		return err
+func createSymlink(srcPath, dstPath string, absolute bool) error {
+	var target string
+	if absolute {
+		target = srcPath
+	} else {
+		relTarget, err := filepath.Rel(filepath.Dir(dstPath), srcPath)
+		if err != nil {
+			return err
+		}
+		target = relTarget
 	}
 
-	target := filepath.Join(relPath, filepath.Base(src))
-	return os.Symlink(target, dst)
+	return os.Symlink(target, dstPath)
 }
 
-func symlinkCopy(srcDir, dstDir string) error {
+func symlinkCopy(srcDir, dstDir string, absolute bool) error {
 	var wg sync.WaitGroup
 	var walkErr error
 
@@ -42,7 +48,7 @@ func symlinkCopy(srcDir, dstDir string) error {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				if err := createSymlink(srcPath, dstPath); err != nil {
+				if err := createSymlink(srcPath, dstPath, absolute); err != nil {
 					walkErr = err
 				}
 			}()
@@ -61,36 +67,25 @@ func symlinkCopy(srcDir, dstDir string) error {
 }
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("Usage: %s <source_dir> <destination_dir>\n", os.Args[0])
+	// Add the "absolute" flag
+	absolute := flag.Bool("absolute", false, "Use absolute paths for symlinks")
+	flag.Parse()
+
+	// Verify the correct number of arguments
+	if len(flag.Args()) != 2 {
+		fmt.Println("Usage: symlink_copy [OPTIONS] <src> <dst>")
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	srcDir, err := filepath.Abs(os.Args[1])
+	// Get the source and destination directories
+	srcDir := flag.Arg(0)
+	dstDir := flag.Arg(1)
+
+	// Run the symlinkCopy function
+	err := symlinkCopy(srcDir, dstDir, *absolute)
 	if err != nil {
-		fmt.Printf("Error: Cannot resolve source directory: %v\n", err)
-		os.Exit(1)
-	}
-
-	dstDir, err := filepath.Abs(os.Args[2])
-	if err != nil {
-		fmt.Printf("Error: Cannot resolve destination directory: %v\n", err)
-		os.Exit(1)
-	}
-
-	if _, err := os.Stat(srcDir); os.IsNotExist(err) {
-		fmt.Println("Error: Source directory does not exist or is not a directory.")
-		os.Exit(1)
-	}
-
-	if _, err := os.Stat(dstDir); os.IsNotExist(err) {
-		fmt.Println("Error: Destination directory does not exist or is not a directory.")
-		os.Exit(1)
-	}
-
-	err = symlinkCopy(srcDir, dstDir)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+		fmt.Printf("Error creating symlink copy: %v\n", err)
 		os.Exit(1)
 	}
 }
--- pkgs/symlink-mirror/src/cmd/symlink-mirror/main_test.go
+++ pkgs/symlink-mirror/src/cmd/symlink-mirror/main_test.go
@@ -8,97 +8,111 @@ import (
 )
 
 func TestSymlinkCopyIntegration(t *testing.T) {
-	// Create temporary source and destination directories.
-	srcDir, err := ioutil.TempDir("", "srcDir")
-	if err != nil {
-		t.Fatalf("Error creating temporary source directory: %v", err)
-	}
-	defer os.RemoveAll(srcDir)
-
-	dstDir, err := ioutil.TempDir("", "dstDir")
-	if err != nil {
-		t.Fatalf("Error creating temporary destination directory: %v", err)
-	}
-	defer os.RemoveAll(dstDir)
-
-	// Create source directory structure and files.
-	testFiles := []string{
-		"file1.txt",
-		"dir1/file2.txt",
-		"dir1/dir2/file3.txt",
-		"dir3/file4.txt",
-		"restrictedDir/file5.txt",
-	}
-
-	for _, file := range testFiles {
-		filePath := filepath.Join(srcDir, file)
-		err := os.MkdirAll(filepath.Dir(filePath), 0755)
+	runTest := func(t *testing.T, absolute bool) {
+		// Create temporary source and destination directories.
+		srcDir, err := ioutil.TempDir("", "srcDir")
 		if err != nil {
-			t.Fatalf("Error creating test directory structure: %v", err)
+			t.Fatalf("Error creating temporary source directory: %v", err)
+		}
+		defer os.RemoveAll(srcDir)
+
+		dstDir, err := ioutil.TempDir("", "dstDir")
+		if err != nil {
+			t.Fatalf("Error creating temporary destination directory: %v", err)
+		}
+		defer os.RemoveAll(dstDir)
+
+		// Create source directory structure and files.
+		testFiles := []string{
+			"file1.txt",
+			"dir1/file2.txt",
+			"dir1/dir2/file3.txt",
+			"dir3/file4.txt",
+			"restrictedDir/file5.txt",
 		}
 
-		err = ioutil.WriteFile(filePath, []byte("test content"), 0644)
-		if err != nil {
-			t.Fatalf("Error creating test file: %v", err)
-		}
-	}
+		for _, file := range testFiles {
+			filePath := filepath.Join(srcDir, file)
+			err := os.MkdirAll(filepath.Dir(filePath), 0755)
+			if err != nil {
+				t.Fatalf("Error creating test directory structure: %v", err)
+			}
 
-	// Set restricted permissions on the "restrictedDir" directory.
-	restrictedDir := filepath.Join(srcDir, "restrictedDir")
-	err = os.Chmod(restrictedDir, 0500)
-	if err != nil {
-		t.Fatalf("Error setting restricted permissions on test directory: %v", err)
-	}
-
-	// Run symlinkCopy function.
-	err = symlinkCopy(srcDir, dstDir)
-	if err != nil {
-		t.Fatalf("Error running symlinkCopy: %v", err)
-	}
-
-	// Verify that the destination directory has the correct symlinks.
-	err = filepath.Walk(dstDir, func(dstPath string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
+			err = ioutil.WriteFile(filePath, []byte("test content"), 0644)
+			if err != nil {
+				t.Fatalf("Error creating test file: %v", err)
+			}
 		}
 
-		if dstPath == dstDir {
+		// Set restricted permissions on the "restrictedDir" directory.
+		restrictedDir := filepath.Join(srcDir, "restrictedDir")
+		err = os.Chmod(restrictedDir, 0500)
+		if err != nil {
+			t.Fatalf("Error setting restricted permissions on test directory: %v", err)
+		}
+
+		// Run symlinkCopy function.
+		err = symlinkCopy(srcDir, dstDir, absolute)
+		if err != nil {
+			t.Fatalf("Error running symlinkCopy: %v", err)
+		}
+
+		// Verify that the destination directory has the correct symlinks.
+		err = filepath.Walk(dstDir, func(dstPath string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if dstPath == dstDir {
+				return nil
+			}
+
+			relPath, err := filepath.Rel(dstDir, dstPath)
+			if err != nil {
+				return err
+			}
+
+			srcPath := filepath.Join(srcDir, relPath)
+
+			if info.IsDir() {
+				if _, err := os.Stat(srcPath); os.IsNotExist(err) {
+					t.Errorf("Directory in destination does not exist in source: %s", dstPath)
+				}
+			} else if info.Mode()&os.ModeSymlink != 0 {
+				linkTarget, err := os.Readlink(dstPath)
+				if err != nil {
+					return err
+				}
+
+				var expectedTarget string
+				if absolute {
+					expectedTarget = srcPath
+				} else {
+					expectedTarget, err = filepath.Rel(filepath.Dir(dstPath), srcPath)
+					if err != nil {
+						return err
+					}
+				}
+
+				if linkTarget != expectedTarget {
+					t.Errorf("Symlink target incorrect: expected %s, got %s", expectedTarget, linkTarget)
+				}
+			} else {
+				t.Errorf("Non-symlink file found in destination directory: %s", dstPath)
+			}
+
 			return nil
-		}
+		})
 
-		relPath, err := filepath.Rel(dstDir, dstPath)
 		if err != nil {
-			return err
+			t.Fatalf("Error verifying destination directory: %v", err)
 		}
-
-		srcPath := filepath.Join(srcDir, relPath)
-
-		if info.IsDir() {
-			if _, err := os.Stat(srcPath); os.IsNotExist(err) {
-				t.Errorf("Directory in destination does not exist in source: %s", dstPath)
-			}
-		} else if info.Mode()&os.ModeSymlink != 0 {
-			linkTarget, err := os.Readlink(dstPath)
-			if err != nil {
-				return err
-			}
-
-			expectedTarget, err := filepath.Rel(filepath.Dir(dstPath), srcPath)
-			if err != nil {
-				return err
-			}
-
-			if linkTarget != expectedTarget {
-				t.Errorf("Symlink target incorrect: expected %s, got %s", expectedTarget, linkTarget)
-			}
-		} else {
-			t.Errorf("Non-symlink file found in destination directory: %s", dstPath)
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		t.Fatalf("Error verifying destination directory: %v", err)
 	}
+
+	t.Run("relative", func(t *testing.T) {
+		runTest(t, false)
+	})
+	t.Run("absolute", func(t *testing.T) {
+		runTest(t, true)
+	})
 }
