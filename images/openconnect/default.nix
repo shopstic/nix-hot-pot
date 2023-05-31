5b862ee09a7a72b7c8ea552ff4e51796cd72cddf
Jacky Nguyen <nktpro@gmail.com>
2023-05-30 21:57:48 -0400

    [image-openconnect] Pin to version 8.20

--- images/openconnect/default.nix
+++ images/openconnect/default.nix
@@ -15,21 +15,29 @@
 , coreutils
 , bash
 , cacert
+, fetchurl
 }:
 let
-  name = openconnect.name;
+  openconnect-8 = openconnect.overrideAttrs (final: prev: rec {
+    version = "8.20";
+    src = fetchurl {
+      url = "ftp://ftp.infradead.org/pub/openconnect/openconnect-${version}.tar.gz";
+      sha256 = "sha256-wUUjhMb3lrruRdTpGa4b/CgdbIiGLh9kaizFE/xE5Ys=";
+    };
+  });
+  name = openconnect-8.name;
   image =
     nix2container.buildImage
       {
         inherit name;
         # fromImage = base-image;
-        tag = openconnect.version;
+        tag = openconnect-8.version;
         copyToRoot = buildEnv {
           name = "bin";
           pathsToLink = [ "/bin" ];
           paths = [
             dumb-init
-            openconnect
+            openconnect-8
             netcat
             iptables
             sysctl
