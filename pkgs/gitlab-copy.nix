842cf09ab8c359a5907a92c4548d265fb697dc38
Jacky Nguyen <nktpro@gmail.com>
2023-08-27 15:08:01 -0400

    [gitlab-copy] Add package

---
+++ pkgs/gitlab-copy.nix
@@ -0,0 +1,25 @@
+{ buildGoModule
+, fetchFromGitHub
+, lib
+}:
+buildGoModule rec {
+  pname = "gitlab-copy";
+  version = "0.8.2";
+
+  src = fetchFromGitHub {
+    owner = "gotsunami";
+    repo = "gitlab-copy";
+    rev = "v${version}";
+    sha256 = "sha256-fzWmJM5oR0AfFgLhHw9+1VHUu1kr2mY081E7kPZq3/c=";
+  };
+
+  vendorSha256 = "sha256-6GnkVKu7QKCYYWDrhVQeEPlZis0g/VEej/dka7n3Dfs=";
+
+  doCheck = false;
+
+  meta = with lib; {
+    description = "GitLab Copy";
+    homepage = "https://github.com/gotsunami/gitlab-copy";
+    license = licenses.mit;
+  };
+}
