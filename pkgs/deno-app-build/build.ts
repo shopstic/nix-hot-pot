84f9efbc7b9b4edc81fbeb34acd4637f736a7677
Jacky Nguyen <nktpro@gmail.com>
2024-04-09 15:06:46 -0400

    In build-deno-app: clean up side effects

--- pkgs/deno-app-build/build.ts
+++ pkgs/deno-app-build/build.ts
@@ -18,6 +18,10 @@ import { createCache } from "https://deno.land/x/deno_cache@0.7.1/mod.ts";
 import { parseFromJson } from "https://deno.land/x/import_map@v0.19.1/mod.ts";
 import { Semaphore } from "jsr:@wok/utils@1.2.0/semaphore";
 
+function isRelativePath(path: string) {
+  return path.startsWith("./") || path.startsWith("../");
+}
+
 const [appPath, outPath] = Deno.args;
 
 if (!appPath) {
@@ -32,181 +36,195 @@ if (!outPath) {
 const absoluteOutPath = resolve(outPath);
 const outToAppRelativePath = relative(absoluteAppPath, absoluteOutPath);
 const rootPath = Deno.cwd();
+const vendorDir = resolve(rootPath, ".vendor");
 
-await inheritExec({
-  cmd: ["deno", "vendor", absoluteAppPath],
-});
-
-const importMapUrl = toFileUrl(resolve("./vendor/import_map.json"));
-const hasImportMap = await exists(importMapUrl);
-
-type Load = NonNullable<TranspileOptions["load"]>;
-type LoadParams = Parameters<Load>;
-
-const cache = createCache({ allowRemote: false });
-
-const result = await transpile(absoluteAppPath, {
-  importMap: hasImportMap ? importMapUrl : undefined,
-  allowRemote: false,
-  async load(
-    specifier: string,
-    isDynamic?: boolean,
-    cacheSetting?: LoadParams[2],
-    checksum?: string,
-  ) {
-    if (specifier.startsWith("node:")) {
-      return {
-        kind: "external",
-        specifier,
-      };
-    }
-    return await cache.load(specifier, isDynamic, cacheSetting, checksum);
-  },
-});
-
-const importMap = hasImportMap
-  ? await parseFromJson(
-    importMapUrl,
-    await Deno.readTextFile(importMapUrl),
-  )
-  : undefined;
-
-function isRelativePath(path: string) {
-  return path.startsWith("./") || path.startsWith("../");
+let backupDenoJson: string | undefined;
+if (await exists(join(rootPath, "deno.json"))) {
+  backupDenoJson = await Deno.readTextFile(join(rootPath, "deno.json"));
 }
 
-const toCopyAssetFiles: string[] = [];
+try {
+  await inheritExec({
+    cmd: ["deno", "vendor", `--output=${vendorDir}`, absoluteAppPath],
+  });
 
-function rewriteModuleSpecifier(path: string, specifier: string) {
-  if (
-    importMap &&
-    !isRelativePath(specifier) &&
-    !specifier.startsWith("node:")
-  ) {
-    const parentPath = dirname(path);
-    const resolved = importMap.resolve(specifier, toFileUrl(path));
-
-    const resolvedRelative = relative(parentPath, fromFileUrl(resolved))
-      .replace(/\.ts$/, ".js");
-
-    if (!isRelativePath(resolvedRelative)) {
-      return `./${resolvedRelative}`;
-    }
-
-    return resolvedRelative;
+  if (backupDenoJson !== undefined) {
+    await Deno.writeTextFile(join(rootPath, "deno.json"), backupDenoJson);
   }
 
-  if (isRelativePath(specifier) && specifier.endsWith(".json")) {
-    toCopyAssetFiles.push(
-      resolve(outToAppRelativePath, resolve(dirname(path), specifier)),
-    );
-  }
+  const importMapUrl = toFileUrl(join(vendorDir, "import_map.json"));
+  const hasImportMap = await exists(importMapUrl);
 
-  return specifier.replace(/\.ts$/, ".js");
-}
+  type Load = NonNullable<TranspileOptions["load"]>;
+  type LoadParams = Parameters<Load>;
 
-function transformFile(filePath: string, sourceCode: string) {
-  const sourceFile = ts.createSourceFile(
-    filePath,
-    sourceCode,
-    ts.ScriptTarget.Latest,
-    true,
-  );
+  const cache = createCache({ allowRemote: false });
 
-  const transformer: ts.TransformerFactory<ts.SourceFile> = (
-    ctx: ts.TransformationContext,
-  ) => {
-    const { factory } = ctx;
-    return (sourceFile: ts.SourceFile) => {
-      function visit(node: ts.Node): ts.Node {
-        if (ts.isImportDeclaration(node)) {
-          if (
-            node.moduleSpecifier && ts.isStringLiteral(node.moduleSpecifier)
-          ) {
-            return factory.updateImportDeclaration(
-              node,
-              node.modifiers,
-              node.importClause,
-              factory.createStringLiteral(
-                rewriteModuleSpecifier(filePath, node.moduleSpecifier.text),
-              ),
-              node.attributes,
-            );
-          }
+  const result = await transpile(absoluteAppPath, {
+    importMap: hasImportMap ? importMapUrl : undefined,
+    allowRemote: false,
+    async load(
+      specifier: string,
+      isDynamic?: boolean,
+      cacheSetting?: LoadParams[2],
+      checksum?: string,
+    ) {
+      if (specifier.startsWith("node:")) {
+        return {
+          kind: "external",
+          specifier,
+        };
+      }
+      return await cache.load(specifier, isDynamic, cacheSetting, checksum);
+    },
+  });
 
-          return node;
-        }
+  const importMap = hasImportMap
+    ? await parseFromJson(
+      importMapUrl,
+      await Deno.readTextFile(importMapUrl),
+    )
+    : undefined;
 
-        if (ts.isExportDeclaration(node)) {
-          if (
-            node.moduleSpecifier && ts.isStringLiteral(node.moduleSpecifier)
-          ) {
-            return factory.updateExportDeclaration(
-              node,
-              node.modifiers,
-              node.isTypeOnly,
-              node.exportClause,
-              factory.createStringLiteral(
-                rewriteModuleSpecifier(filePath, node.moduleSpecifier.text),
-              ),
-              node.attributes,
-            );
-          }
+  const toCopyAssetFiles: string[] = [];
 
-          return node;
-        }
+  const rewriteModuleSpecifier = (path: string, specifier: string) => {
+    if (
+      importMap &&
+      !isRelativePath(specifier) &&
+      !specifier.startsWith("node:")
+    ) {
+      const parentPath = dirname(path);
+      const resolved = importMap.resolve(specifier, toFileUrl(path));
 
-        return ts.visitEachChild(node, visit, ctx);
+      const resolvedRelative = relative(parentPath, fromFileUrl(resolved))
+        .replace(/\.ts$/, ".js");
+
+      if (!isRelativePath(resolvedRelative)) {
+        return `./${resolvedRelative}`;
       }
 
-      const ret = ts.visitNode(sourceFile, visit);
-      assert(ts.isSourceFile(ret));
-      return ret;
-    };
+      return resolvedRelative;
+    }
+
+    if (isRelativePath(specifier) && specifier.endsWith(".json")) {
+      toCopyAssetFiles.push(
+        resolve(outToAppRelativePath, resolve(dirname(path), specifier)),
+      );
+    }
+
+    return specifier.replace(/\.ts$/, ".js");
   };
 
-  const ret = ts.transform(sourceFile, [transformer]);
-  assert(ret.transformed.length === 1);
-  const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-  return printer.printFile(ret.transformed[0]);
-}
+  const transformFile = (filePath: string, sourceCode: string) => {
+    const sourceFile = ts.createSourceFile(
+      filePath,
+      sourceCode,
+      ts.ScriptTarget.Latest,
+      true,
+    );
 
-const sem = new Semaphore(32);
-const promises = Array.from(result).map(async ([key, content]) => {
-  const path = fromFileUrl(key);
-  const newPath = join(
-    absoluteOutPath,
-    relative(rootPath, path).replace(/\.ts$/, ".js"),
-  );
-  const newParentDir = dirname(newPath);
-  await sem.acquire();
-  try {
-    await Deno.mkdir(newParentDir, { recursive: true });
-    await Deno.writeTextFile(newPath, transformFile(path, content));
-  } finally {
-    sem.release();
-  }
-  console.error(`Wrote ${newPath}`);
-});
-await Promise.all(promises);
+    const transformer: ts.TransformerFactory<ts.SourceFile> = (
+      ctx: ts.TransformationContext,
+    ) => {
+      const { factory } = ctx;
+      return (sourceFile: ts.SourceFile) => {
+        function visit(node: ts.Node): ts.Node {
+          if (ts.isImportDeclaration(node)) {
+            if (
+              node.moduleSpecifier && ts.isStringLiteral(node.moduleSpecifier)
+            ) {
+              return factory.updateImportDeclaration(
+                node,
+                node.modifiers,
+                node.importClause,
+                factory.createStringLiteral(
+                  rewriteModuleSpecifier(filePath, node.moduleSpecifier.text),
+                ),
+                node.attributes,
+              );
+            }
 
-if (toCopyAssetFiles.length > 0) {
-  const promises = toCopyAssetFiles.map(async (path) => {
-    const newPath = join(absoluteOutPath, relative(rootPath, path));
+            return node;
+          }
+
+          if (ts.isExportDeclaration(node)) {
+            if (
+              node.moduleSpecifier && ts.isStringLiteral(node.moduleSpecifier)
+            ) {
+              return factory.updateExportDeclaration(
+                node,
+                node.modifiers,
+                node.isTypeOnly,
+                node.exportClause,
+                factory.createStringLiteral(
+                  rewriteModuleSpecifier(filePath, node.moduleSpecifier.text),
+                ),
+                node.attributes,
+              );
+            }
+
+            return node;
+          }
+
+          return ts.visitEachChild(node, visit, ctx);
+        }
+
+        const ret = ts.visitNode(sourceFile, visit);
+        assert(ts.isSourceFile(ret));
+        return ret;
+      };
+    };
+
+    const ret = ts.transform(sourceFile, [transformer]);
+    assert(ret.transformed.length === 1);
+    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+    return printer.printFile(ret.transformed[0]);
+  };
+
+  const sem = new Semaphore(32);
+  const promises = Array.from(result).map(async ([key, content]) => {
+    const path = fromFileUrl(key);
+    const newPath = join(
+      absoluteOutPath,
+      relative(rootPath, path).replace(/\.ts$/, ".js"),
+    );
     const newParentDir = dirname(newPath);
     await sem.acquire();
     try {
       await Deno.mkdir(newParentDir, { recursive: true });
-      await Deno.copyFile(path, newPath);
+      await Deno.writeTextFile(newPath, transformFile(path, content));
     } finally {
       sem.release();
     }
-    console.error(`Copied ${path} to ${newPath}`);
+    console.error(`Wrote ${newPath}`);
   });
   await Promise.all(promises);
-}
 
-console.log(join(
-  absoluteOutPath,
-  relative(rootPath, absoluteAppPath).replace(/\.ts$/, ".js"),
-));
+  if (toCopyAssetFiles.length > 0) {
+    const promises = toCopyAssetFiles.map(async (path) => {
+      const newPath = join(absoluteOutPath, relative(rootPath, path));
+      const newParentDir = dirname(newPath);
+      await sem.acquire();
+      try {
+        await Deno.mkdir(newParentDir, { recursive: true });
+        await Deno.copyFile(path, newPath);
+      } finally {
+        sem.release();
+      }
+      console.error(`Copied ${path} to ${newPath}`);
+    });
+    await Promise.all(promises);
+  }
+
+  console.log(join(
+    absoluteOutPath,
+    relative(rootPath, absoluteAppPath).replace(/\.ts$/, ".js"),
+  ));
+} finally {
+  try {
+    await Deno.remove(vendorDir, { recursive: true });
+  } catch {
+    // Ignore
+  }
+}
