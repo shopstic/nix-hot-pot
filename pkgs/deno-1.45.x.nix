983ba0224c4e18677fd6613042edda37943f4c5f
Jacky Nguyen <nktpro@gmail.com>
2024-07-12 16:05:40 -0400

    [deno] Update to 1.45.1

--- pkgs/deno-1.45.x.nix
+++ pkgs/deno-1.45.x.nix
@@ -1,19 +1,19 @@
 { autoPatchelfHook, fetchzip, stdenv, lib, makeWrapper }:
 (import ./deno.nix rec {
   inherit autoPatchelfHook fetchzip stdenv lib makeWrapper;
-  version = "1.45.0";
+  version = "1.45.1";
   downloadMap = {
     x86_64-linux = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/deno-x86_64-unknown-linux-gnu.zip";
-      hash = "sha256-gHhNMDk5rEVqeP13w3aYrZwNIJbQKZ5xMrPzObDwA/I=";
+      hash = "sha256-srGJmk+uijWRUn7ay+v2l6Qd0d7TX3j06BjF3maqdIw=";
     };
     aarch64-darwin = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/deno-aarch64-apple-darwin.zip";
-      hash = "sha256-AW8jLL9xelC4IBuhvBK77gPUxd0a7cvXkAw3ooS2GzQ=";
+      hash = "sha256-94yzCFPvIVxhlCDxJrEihaihI+pa9GqBEgAlqw1/3UA=";
     };
     aarch64-linux = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/deno-aarch64-unknown-linux-gnu.zip";
-      hash = "sha256-B45iD77eWMtUZPwO13eNg8G4Ng+G+Z9NTrXDBT5gUwo=";
+      hash = "sha256-JcXVTyyUtY16cP2f6gF7ROqcAzNnIJat4EXP5oQn1PI=";
     };
   };
   priority = 10;
--- pkgs/denort-1.45.x.nix
+++ pkgs/denort-1.45.x.nix
@@ -1,19 +1,19 @@
 { autoPatchelfHook, fetchzip, stdenv, lib, makeWrapper }:
 (import ./denort.nix rec {
   inherit autoPatchelfHook fetchzip stdenv lib makeWrapper;
-  version = "1.45.0";
+  version = "1.45.1";
   downloadMap = {
     x86_64-linux = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/denort-x86_64-unknown-linux-gnu.zip";
-      hash = "sha256-m6StZPQK4WSxry0mDiOm+Ezj3HKxOWxMoPeYQV81ptk=";
+      hash = "sha256-zHixjqb/s7cQcZ0Prf8lBkDXhVuf486OeX85dESgOF4=";
     };
     aarch64-darwin = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/denort-aarch64-apple-darwin.zip";
-      hash = "sha256-HoPnEFbEpYtRod4poBq+QrH1EcSlAwuz9WIqZRhMzno=";
+      hash = "sha256-A1HoeQr3bxTprOU1kxbqFS1SvgmM2bUiaKqf1vMNgM0=";
     };
     aarch64-linux = {
       url = "https://github.com/denoland/deno/releases/download/v${version}/denort-aarch64-unknown-linux-gnu.zip";
-      hash = "sha256-p5Trj87Mk6+ZP/aLoKVMR9vJHPoX6VZR6Qk8d+pjD+o=";
+      hash = "sha256-AcZfyGdXWpULNnyqzeGOIvLGBAZaM+l3i8UXJILXMR0=";
     };
   };
   priority = 10;
