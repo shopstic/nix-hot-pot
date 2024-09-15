5abd1446fccde631cad8a276e3fe296e86d4decd
Jacky Nguyen <nktpro@gmail.com>
2024-09-15 18:17:20 -0400

    [libsqlite] Fix name typo

--- pkgs/libsqlite.nix
+++ pkgs/libsqlite.nix
@@ -1,5 +1,5 @@
 { runCommand, sqlite }:
-runCommand "libpq" { } ''
+runCommand "libsqlite" { } ''
   mkdir -p $out
   find ${sqlite.out}/lib \( -name "libsqlite3.so" -or -name "libsqlite3.dylib" \) -exec cp {} $out/ \;
 ''
