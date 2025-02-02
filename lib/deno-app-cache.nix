4c48c7a3bdb175197c9dfc53f7dbe1951c30e595
Jacky Nguyen <nktpro@gmail.com>
2025-02-01 22:41:11 -0500

    [deno-app-cache] move preCache

--- lib/deno-app-cache.nix
+++ lib/deno-app-cache.nix
@@ -16,12 +16,12 @@ runCommand "${name}-cache"
   export DENO_DIR="$out"
   mkdir -p "$DENO_DIR"
 
-  ${preCache}
   cp --reflink=auto ${cache-entry-file} "./cache-entry.ts"
   chmod +w "./cache-entry.ts"
   cp --reflink=auto ${config-file} "./deno.json"
   ${if lock-file != null then ''cp --reflink=auto ${lock-file} "./deno.lock"'' else ""}
-
+  
+  ${preCache}
   deno check ${denoCheckFlags} --lock=deno.lock ./cache-entry.ts
   ${postCache}
 ''
