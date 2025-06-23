d7b1aec18fc97e1619eb9d7ae14b8816fb07ae2b
Jacky Nguyen <nktpro@gmail.com>
2025-06-23 11:34:30 +0800

    Update dependencies

--- pkgs/grpc-health-probe.nix
+++ pkgs/grpc-health-probe.nix
@@ -1,20 +1,20 @@
-{ buildGo122Module
+{ buildGo123Module
 , fetchFromGitHub
 , lib
 }:
 let
-  grpc-health-probe = buildGo122Module rec {
+  grpc-health-probe = buildGo123Module rec {
     pname = "grpc-health-probe";
-    version = "0.4.26";
+    version = "0.4.38";
 
     src = fetchFromGitHub {
       owner = "grpc-ecosystem";
       repo = "grpc-health-probe";
       rev = "v${version}";
-      sha256 = "sha256-4/Yauqc0ZUhIVoLkDcEhvjX/5ZQxqmfQJTMdLqx48nc=";
+      sha256 = "sha256-0XL/VtgnVfVcb9H8eSFTFCTc9aVl/staABo5iXjT91U=";
     };
 
-    vendorHash = "sha256-Pb1IxZMK0R1IdZouw4XI79gL+ABs/VBejXcRlMa6FC8=";
+    vendorHash = "sha256-p8XABZKLMoyFQfWQWStP0jyrFeh8wofRord0VokQhdY=";
 
     meta = with lib; {
       description = "A command-line tool to perform health-checks for gRPC applications in Kubernetes etc.";
