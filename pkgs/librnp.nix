aeed6b4cc55b14bd0aad5cf0e78b017977b2be77
Jacky Nguyen <nktpro@gmail.com>
2025-06-23 12:23:32 +0800

    Update dependencies

--- pkgs/librnp.nix
+++ pkgs/librnp.nix
@@ -1,5 +1,16 @@
-{ runCommand, rnp }:
+{ runCommand, rnp, fetchFromGitHub }:
+let
+  overriddenRnp = rnp.overrideAttrs (oldAttrs: rec {
+    version = "0.18.0";
+    src = fetchFromGitHub {
+      owner = "rnpgp";
+      repo = "rnp";
+      rev = "v${version}";
+      hash = "sha256-DixJhN4/iSgMkcJ0run4gwFZoUeMMiNSwWDGjT94cNM=";
+    };
+  });
+in
 runCommand "librnp" { } ''
   mkdir -p $out
-  find ${rnp.lib}/lib \( -name "librnp.so" -or -name "librnp.dylib" \) -exec cp {} $out/ \;
+  find ${overriddenRnp.lib}/lib \( -name "librnp.so" -or -name "librnp.dylib" \) -exec cp {} $out/ \;
 ''
