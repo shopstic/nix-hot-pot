da0d58244615e961b79f08f3990642eca0878bfe
Jacky Nguyen <nktpro@gmail.com>
2025-06-23 12:12:34 +0800

    Update jdk17

--- pkgs/jdk17/default.nix
+++ pkgs/jdk17/default.nix
@@ -1,39 +1,5 @@
-{ stdenv, jdk17, jdk17_headless, fetchurl }:
-if stdenv.isDarwin then
-  let
-    dist = {
-      aarch64-darwin = {
-        arch = "aarch64";
-        zuluVersion = "17.52.17";
-        jdkVersion = "17.0.12";
-        jdkSha256 = "sha256-RZ3hNQQFE+spTT9lFHLjmkolSkpHDg2Hvro6xi7uNww=";
-        jreSha256 = "sha256-cw5o8SEOixQpu0d0Syzl6reveSsuaOTYhsdicKzkvew=";
-      };
-    }."${stdenv.hostPlatform.system}";
-  in
-  {
-    jdk = jdk17.overrideAttrs (oldAttrs: {
-      pname = "zulu${dist.zuluVersion}-ca-jdk";
-      version = dist.jdkVersion;
-      src = fetchurl {
-        url = "https://cdn.azul.com/zulu/bin/zulu${dist.zuluVersion}-ca-jdk${dist.jdkVersion}-macosx_${dist.arch}.tar.gz";
-        sha256 = dist.jdkSha256;
-        curlOpts = "-H Referer:https://www.azul.com/downloads/zulu/";
-      };
-    });
-    jre = jdk17.overrideAttrs (oldAttrs: {
-      pname = "zulu${dist.zuluVersion}-ca-jre";
-      version = dist.jdkVersion;
-      src = fetchurl {
-        url = "https://cdn.azul.com/zulu/bin/zulu${dist.zuluVersion}-ca-jre${dist.jdkVersion}-macosx_${dist.arch}.tar.gz";
-        sha256 = dist.jreSha256;
-        curlOpts = "-H Referer:https://www.azul.com/downloads/zulu/";
-      };
-      postFixup = "";
-    });
-  }
-else
-  {
-    jdk = jdk17;
-    jre = jdk17_headless;
-  }
+{ stdenv, jdk17, jdk17_headless }:
+{
+  jdk = jdk17;
+  jre = jdk17_headless;
+}
